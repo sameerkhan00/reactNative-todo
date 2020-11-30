e40a25824c5f4a1319b957489597ad031948d190
Muhammad Sameer Khan <63584585+sameerkhan00@users.noreply.github.com>
2020-12-01 01:27:56 +0500

    todo

--- App.js
+++ App.js
@@ -1,21 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import Note from './app/component/Note'
+
+import { ScrollView, StyleSheet, Text, TextInput, View,SafeAreaView, TouchableOpacity } from 'react-native';
+import Main from './app/component/Main';
+
+export default class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      noteArray:[],
+      noteText:'',
+    }
+  }
+  
+  render() {
+    let notes=this.state.noteArray.map((val,key)=>{
+      return<Note key={key} keyval={key} val={val}
+      deleteMethod={()=>this.deleteNote(key)}/>
+    })
+    return (
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.header}>
+          <Text style={styles.headerText}>TO DO</Text>
+        </View>
+        <ScrollView style={styles.scrollContainer}>
+      {notes}
+        </ScrollView>
+
+        <View style={styles.footer}>
+         
+         <TextInput value={this.state.noteText} onChangeText={(noteText)=>this.setState({noteText})} style={styles.textInput} placeholder="Type here ..." >
+          </TextInput>
+          <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+          <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+      
+        </View>
+        
+      </SafeAreaView>
+      
+
+    );
+  }
+  deleteNote (key){
+    this.state.noteArray.splice(key,1);
+    this.setState({noteArray: this.state.noteArray})
+  }
+  addNote(){
+   if (this.state.noteText ){
+     var d=new Date();
+     this.state.noteArray.push({
+       'date':d.getFullYear()+'/'
+       +(d.getMonth()+1)+'/'+d.getDate(),
+       'note':this.state.noteText
+     });
+     this.setState({noteArray: this.state.noteArray})
+     this.setState({noteText: ''});
+   }
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+  
   },
+  header: {
+    backgroundColor: '#E91E63',
+    alignItems: 'center',
+    justifyContent: "center",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ddd",
+    
+  }
+  ,
+  headerText: {
+    color: 'white',
+    fontSize: 18,
+    padding: 26,
+    fontWeight:'bold',
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100,
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    color: 'black',
+    padding: 20,
+    // backgroundColor: '#252525',
+    borderTopWidth: 2,
+    borderTopColor: '#ededed',
+    marginBottom:1,
+    height:80
+
+  },
+  addButton: {
+    position: 'absolute',
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    backgroundColor: "#E91E63",
+    width: 90,
+    height: 90,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius:50,
+    // elevation: B,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 24,
+  }
+
 });
---
+++ app/component/Main.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { StyleSheet, Text, View,TextInput,ScrollView,TouchableOpacity } from 'react-native';
+
+export default class Main extends React.Component {
+    render(){
+  return (
+    <View style={styles.container}>
+    <Text>Testing</Text>
+    
+    </View>
+  );
+}}
+
+const styles = StyleSheet.create({
+  container: {
+      
+  },
+});
